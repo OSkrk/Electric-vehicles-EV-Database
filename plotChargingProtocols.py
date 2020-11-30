6008790728fbb71e814109b8d45afdd49b6e0fb0
OSkrk <63248433+OSkrk@users.noreply.github.com>
2020-12-01 00:02:53 +0100

    Update plotChargingProtocols.py

--- plotChargingProtocols.py
+++ plotChargingProtocols.py
@@ -2,42 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import csv
+# import CSV file
 with open('EVs_data_base.csv', 'r', newline='') as f:
     rows = list(csv.reader(f, delimiter=','))
     for row in rows[1:]:
-        row[5]= int(row[5])
-    
+        row[5]= int(row[5])# transform column from string to int values
+# Vectors initialization  
 years=[]
 chargingType=[]
+
 for row in rows[1:]:
     if row[5]!='':years.append(row[5])
     if row[2]!='None': chargingType.append(row[2])
     if row[6]!='None': chargingType.append(row[6])
-chargingType=list(dict.fromkeys(chargingType))
+chargingType=list(dict.fromkeys(chargingType)) # remove repeated items
 chargingType.sort()
-years=list(dict.fromkeys(years))
+years=list(dict.fromkeys(years)) # remove repeated items
 years.sort()
-print (chargingType)
+## Gathering data for plotting
 x=[]
 y=[]
 z=[]
+d={}
+i=0
 for entry in years:
     for elements in chargingType:
         x.append(entry)
         y.append(elements)
-        z.append(1)
-index=0
-for year in years:
-    for chtype in chargingType:
-        for row in rows:
-            if row[5]==year and row [2]==chtype:
-                z[index]=z[index]+20
-            if row[5]==year and row [6]==chtype:
-                z[index]=z[index]+20
-        index=index+1
+        z.append(0)
+        d[repr([entry,elements])]=i
+        i=i+1
+entry=0
+for row in rows: # For all rows
+    entry=d.get(repr([row[5],row[2]])) # DC supported protocol and year
+    if entry is not None:
+        z[entry]=z[entry]+20
+    entry=d.get(repr([row[5],row[6]])) # AC supported protocol and year
+    if entry is not None:
+        z[entry]=z[entry]+20
 
-print(len(x),len(y))
-#z=# Count of cars
 fig = go.Figure(data=[go.Scatter(
     x=x, y=y,
     mode='markers',
